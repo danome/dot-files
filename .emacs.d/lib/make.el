db7f4b607d6376a27ee209d734f1a9bba1f01c00
Eric B. Decker <cire831@gmail.com>
2018-01-21 22:54:42 -0800

    make.el: remove environment_bash (causes problems)
    you mileage will vary.  Example left in if it doesn't work.

--- .emacs.d/lib/make.el
+++ .emacs.d/lib/make.el
@@ -1,8 +1,16 @@
+;;;
+;;; you may or may not need the source of .environment_bash
+;;; not sure what is going on there.
+;;;
+
 (setq make-command ". ~/.environment_bash; make -k -Wall")
 (setq make-command "gcc -o align -ggdb -Wall -m32 align.c")
-(setq make-command ". ~/.environment_bash; make mm6a")
-(setq gud-gdb-command-name "arm-none-eabi-gdb-py -i=mi build/mm6a/main.exe")
-(setq make-command ". ~/.environment_bash; make dev6a")
-(setq gud-gdb-command-name "arm-none-eabi-gdb-py -i=mi build/dev6a/main.exe")
-(setq make-command ". ~/.environment_bash; make dev6a")
-(setq gud-gdb-command-name "arm-none-eabi-gdb-py -i=mi -iex \"target remote localhost:2331\" main.exe")
+
+(setq gud-gdb-command-name "gdb -i=mi")
+(setq make-command "make -k")
+
+(setq make-command "make mm6a")
+(setq gud-gdb-command-name "arm-none-eabi-gdb-py -i=mi -iex \"target extended-remote localhost:2331\" -cd build/mm6a/ main.exe")
+
+(setq make-command "make dev6a")
+(setq gud-gdb-command-name "arm-none-eabi-gdb-py -i=mi -iex \"target extended-remote localhost:2331\" -cd build/dev6a/ main.exe")
