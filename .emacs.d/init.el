584936a906faa3b917b0dcee1092dcccd7faee86
Daniel J. Maltbie <dmaltbie@daloma.org>
2020-06-24 17:10:50 -0700

    Install magit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -500,3 +500,30 @@
  ;; If there is more than one, they won't work right.
  '(magit-commit-arguments nil)
  '(magit-log-arguments (quote ("--graph" "--decorate" "-n256"))))
+
+(require 'package)
+;; load packages ourselves
+;; press C-h v on the variable for more documentation
+(setq package-enable-at-startup nil)
+;; Add MELPA to package-archives
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; load packages now
+(package-initialize)
+
+;; Emacs adds `custom' settings in the init file by default. Run this file
+;; without this segment to see what that means.
+;; Put those away in "custom.el".
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file :noerror)
+
+;; update local database then install use-package if it's not installed
+(unless (package-installed-p 'use-package)
+ (package-refresh-contents)
+ (package-install 'use-package))
+
+(require 'use-package)
+;; tell use-package to install a package if it's not already installed
+(setq use-package-always-ensure t)
+
+(use-package magit
+ :bind (("C-x g" . magit)))
