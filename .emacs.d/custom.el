d2ff50ce772289e90bb69828bd7941058e63037e
Dan Maltbie <dmaltbie@daloma.org>
2022-02-02 21:53:39 -0800

    add custom-set-variables

---
+++ .emacs.d/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-commit-arguments nil)
+ '(magit-log-arguments (quote ("--graph" "--decorate" "-n256")))
+ '(package-selected-packages
+   (quote
+    (elpy cider clojure-mode-extra-font-locking clojure-mode exec-path-from-shell ghub magit-popup memoize paredit parseedn pkg-info projectile queue rainbow-delimiters sesman smex tagedit company-graphviz-dot graphviz-dot-mode markdown-preview-mode markdown-mode magit use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Ubuntu Mono")))))
